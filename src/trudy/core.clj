7715ed278dbd3558e5151bef3c1ab1616f1fd03e
James Reeves <jreeves@weavejester.com>
2013-10-18 08:43:35 -0400

    Basic 'hello world' graphics

--- src/trudy/core.clj
+++ src/trudy/core.clj
@@ -1,10 +1,21 @@
 (ns trudy.core
-  (:import javax.swing.JFrame))
+  (:import [javax.swing JFrame JPanel]
+           [java.awt Graphics2D]))
 
-(defn frame [title [width height]]
+(defn drawable-panel [painter]
+  (proxy [JPanel] []
+    (paintComponent [graphics]
+      (proxy-super paintComponent graphics)
+      (painter graphics))))
+
+(defn frame [title [width height] painter]
   (doto (JFrame. title)
     (.setSize width height)
+    (.add (drawable-panel painter))
     (.setVisible true)))
 
+(defn draw-example [^Graphics2D graphics]
+  (.drawString graphics "Hello World" 10 10))
+
 (defn -main [& args]
-  (frame "Demo" [200 200]))
+  (frame "Demo" [200 200] draw-example))
