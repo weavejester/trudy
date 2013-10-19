31617363de33bd3d4005c0538cf42bbb35379d1d
James Reeves <jreeves@weavejester.com>
2013-10-19 08:51:57 -0400

    Moved render-loop into Swing engine

--- src/trudy/core.clj
+++ src/trudy/core.clj
@@ -1,54 +1,9 @@
 (ns trudy.core
-  (:import [java.awt Canvas Color Graphics2D RenderingHints])
+  (:import java.awt.Graphics2D)
   (:require [trudy.swing :as swing]))
-
-(defn- frame-sleep-time [frame-start frame-length]
-  (let [duration (- (System/currentTimeMillis) frame-start)]
-    (max 0 (- frame-length duration))))
-
-(defn- set-font-hints! [^Graphics2D g]
-  (.setRenderingHint g RenderingHints/KEY_TEXT_ANTIALIASING
-                       RenderingHints/VALUE_TEXT_ANTIALIAS_GASP))
-
-(defn- clear-canvas! [^Canvas canvas ^Graphics2D graphics]
-  (let [w (.getWidth canvas)
-        h (.getHeight canvas)]
-    (.setColor graphics Color/white)
-    (.fillRect graphics 0 0 w h)
-    (.setColor graphics Color/black)))
-
-(defn paint-canvas [canvas painter]
-  (let [strategy (.getBufferStrategy canvas)
-        graphics (.getDrawGraphics strategy)]
-    (set-font-hints! graphics)
-    (clear-canvas! canvas graphics)
-    (painter graphics)
-    (.dispose graphics)
-    (.show strategy)))
-
-(defn paint-loop
-  ([canvas painter]
-     (paint-loop canvas painter 60))
-  ([canvas painter fps]
-     (let [frame-length (/ 1000.0 fps)]
-       (loop [time-prev nil]
-         (let [time-start (System/currentTimeMillis)]
-           (paint-canvas canvas painter)
-           (Thread/sleep (frame-sleep-time time-start frame-length))
-           (recur time-start))))))
-
-(defn window [title size painter]
-  (let [canvas (Canvas.)]
-    (swing/window title size canvas)
-    (doto canvas
-      (.createBufferStrategy 2)
-      (.setIgnoreRepaint true)
-      (.requestFocus))
-    (future
-      (paint-loop canvas painter 60))))
 
 (defn hello-world [^Graphics2D graphics]
   (.drawString graphics "Hello World" 10 10))
 
 (defn -main [& args]
-  (window "Demo" [200 200] hello-world))
+  (swing/run-app hello-world {:title "Demo" :size [200 200]}))
--- src/trudy/swing.clj
+++ src/trudy/swing.clj
@@ -1,12 +1,57 @@
 (ns trudy.swing
   "Create graphics window with Swing."
   (:import [javax.swing JFrame JPanel]
-           [java.awt Dimension]))
+           [java.awt Dimension Canvas Color Graphics2D RenderingHints]))
 
-(defn window [title [width height] canvas]
+(defn- clear-canvas! [^Canvas canvas ^Graphics2D graphics]
+  (let [w (.getWidth canvas)
+        h (.getHeight canvas)]
+    (.setColor graphics Color/white)
+    (.fillRect graphics 0 0 w h)
+    (.setColor graphics Color/black)))
+
+(defn- set-font-hints! [^Graphics2D g]
+  (.setRenderingHint g RenderingHints/KEY_TEXT_ANTIALIASING
+                       RenderingHints/VALUE_TEXT_ANTIALIAS_GASP))
+
+(defn paint-canvas [canvas painter]
+  (let [strategy (.getBufferStrategy canvas)
+        graphics (.getDrawGraphics strategy)]
+    (set-font-hints! graphics)
+    (clear-canvas! canvas graphics)
+    (painter graphics)
+    (.dispose graphics)
+    (.show strategy)))
+
+(defn- frame-sleep-time [frame-start frame-length]
+  (let [duration (- (System/currentTimeMillis) frame-start)]
+    (max 0 (- frame-length duration))))
+
+(defn paint-loop
+  ([canvas painter]
+     (paint-loop canvas painter 60))
+  ([canvas painter fps]
+     (let [frame-length (/ 1000.0 fps)]
+       (loop [time-prev nil]
+         (let [time-start (System/currentTimeMillis)]
+           (paint-canvas canvas painter)
+           (Thread/sleep (frame-sleep-time time-start frame-length))
+           (recur time-start))))))
+
+(defn frame [title [width height] canvas]
   (let [frame (JFrame. title)
         panel (.getContentPane frame)]
     (.setPreferredSize panel (Dimension. width height))
     (.add panel canvas)
     (.pack frame)
     (.setVisible frame true)))
+
+(defn run-app [painter {:keys [title size]}]
+  (let [canvas (Canvas.)]
+    (frame title size canvas)
+    (doto canvas
+      (.createBufferStrategy 2)
+      (.setIgnoreRepaint true)
+      (.requestFocus))
+    (future
+      (paint-loop canvas painter 60))))
