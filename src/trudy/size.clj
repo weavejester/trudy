cf285e8c3bacae7dfd82bdbaeb281a0ed0cc29a7
James Reeves <jreeves@weavejester.com>
2013-11-13 15:18:03 +0000

    Added size namespace

---
+++ src/trudy/size.clj
@@ -0,0 +1,27 @@
+(ns trudy.size
+  (:refer-clojure :exclude [min max range])
+  (:require [clojure.core :as core]
+            [trudy.macros :refer (set-print-methods!)]))
+
+(defprotocol Bounds
+  (min [x])
+  (max [x]))
+
+(deftype Range [start end]
+  Bounds
+  (min [_] start)
+  (max [_] end)
+  clojure.lang.Seqable
+  (seq [_] (core/range start end))
+  Object
+  (toString [_] (str "#range " (pr-str [start end]))))
+
+(set-print-methods! Range)
+
+(defn range [start end]
+  (Range. start end))
+
+(extend-type Number
+  Bounds
+  (min [x] x)
+  (max [x] x))
